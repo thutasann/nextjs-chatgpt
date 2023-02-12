1a205515f568e1aa8375e5baba41a868cfeb3237
thutasann <thutasann2002@gmail.com>
2023-02-12 22:50:29 +0630

    Feature: SignIn SignOut

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,6 +1,11 @@
+'use client';
+
+import { useSession, signOut } from 'next-auth/react';
 import NewChat from './newchat';
 
 function SideBar() {
+	const { data: session } = useSession();
+
 	return (
 		<div className="p-2 flex flex-col min-h-screen">
 			<div className="flex-1">
@@ -10,6 +15,17 @@ function SideBar() {
 					{/* ChatRows */}
 				</div>
 			</div>
+
+			{/* Profile */}
+			{session && (
+				/* eslint-disable @next/next/no-img-element */
+				<img
+					onClick={() => signOut()}
+					src={session.user?.image!}
+					alt={session?.user?.name!}
+					className="h-12 w-12 rounded-full cursor-pointer mx-auto mb-6 hover:opacity-50"
+				/>
+			)}
 		</div>
 	);
 }
